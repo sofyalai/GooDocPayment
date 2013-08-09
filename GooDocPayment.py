95e16cdc779ab7662be3c9305c357235e66ab774
sofyalai <sofyalai@gmail.com>
2013-08-09 16:10:05 +0200

    2nd commit

--- GooDocPayment.py
+++ GooDocPayment.py
@@ -2,36 +2,63 @@
 
 import sys
 import gspread
+import datetime
+import webbrowser
 
 total = len(sys.argv)
-
-cmdargs = str(sys.argv)
-
-# print ("The total number of args pass:%d" % total)
-# print ("Args list:%s" % cmdargs)
-
-# print ("script name : %s" % str(sys.argv[0]))
-# for i in xrange(total):
-#     print ("arg # %d: %s" % (i,str(sys.argv[i])))
 
 # User input
 account_input = raw_input('your account: ')
 password_input = raw_input('your password: ')
+gc = gspread.login(account_input, password_input) 
 
-gc = gspread.login(account_input, password_input)
-
+# assign worksheet
 sht1 = gc.open_by_key('0Al7r6sbzIbfsdGhTcEJ1SEpkZnVMTTBJc0JVYlc5NVE')
-
-# Select worksheet by index. Worksheet indexes start from zero
-worksheet = sht1.get_worksheet(0)
-
-# Most common case: Sheet1
 worksheet = sht1.sheet1
 
-val = worksheet.acell('D2').value
+# fetch values of the 1st colum
+values_list = worksheet.col_values(1)
 
-print val
+# update data main function
+def update_Data():
+    # assign col
+    if str(sys.argv[1])=='f':
+        col=2
+    elif str(sys.argv[1])=='y':
+        col=3
+
+    # assign row
+    row = len(values_list)+1
+
+    # insert 1 row
+    worksheet.add_rows(1)
+    # assign now_date
+    if total == 4:
+        now_date = str(sys.argv[3])
+    else:
+        localtime = datetime.datetime.now()
+        now_date = "%d-%d-%d" % (localtime.year, localtime.month, localtime.day)
+    # input date to cell
+    worksheet.update_cell(row, 1, now_date)
+
+    # input cash amount to cell
+    worksheet.update_cell(row, col, str(sys.argv[2]))
+            
+    # fetch the total value from worksheet
+    # conver val to float number)
+    val = worksheet.acell('D2').value
+    if val>0 :
+        print ("Ying-Chun owes :%s kr" % val)
+    else:
+        print ("Fredrik owes :%s kr" % val)
 
 
 
+if str(sys.argv[1]) == "rm" : #remove last row
+    worksheet.resize(len(values_list)-1)
+elif str(sys.argv[1]) == "open" : # open sheet in firefox
+    controller = webbrowser.get('Firefox')
+    controller.open('https://docs.google.com/spreadsheet/ccc?key=0Al7r6sbzIbfsdGhTcEJ1SEpkZnVMTTBJc0JVYlc5NVE#gid=0')
+else: # update data
+    update_Data();
 
